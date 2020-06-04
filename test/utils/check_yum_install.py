cdee2eb4ecbb5156757ed4016757f2d925c64b5a
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2020-06-04 17:18:02 +0200

    Create install info file (#52)
    * Add install_info
    
    * [CI] Add tests for install_info to CI

--- test/utils/check_yum_install.py
+++ test/utils/check_yum_install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import yum, sys
-from helpers import get_options, check_major_version
+from helpers import get_options, check_major_version, check_install_info
 
 def get_yum_package_version(package_name):
   yb = yum.YumBase()
@@ -15,6 +15,7 @@ def get_yum_package_version(package_name):
   
   return installed_version
 
+
 def main(argv):
   expected_major_version = get_options(argv[1:])
   print("Expected major version: {}".format(expected_major_version))
@@ -25,10 +26,22 @@ def main(argv):
   result = check_major_version(installed_version, expected_major_version)
   if result:
     print("Agent version check successful!")
-    sys.exit()
   else:
     print("Agent version check failed.")
     sys.exit(1)
+
+  # expected_major_version
+  if expected_major_version:
+    if check_install_info(expected_major_version):
+      print("install_info check successful!")
+    else:
+      print("install_info check failed.")
+      sys.exit(1)
+  else:
+    print("Skipping install_info check.")
+
+  sys.exit()
+
 
 if __name__ == "__main__":
   main(sys.argv)
--- test/utils/helpers.py
+++ test/utils/helpers.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import getopt, re
+import getopt
+import re
+import os.path
+import os
 
 def get_options(args):
   expected_major_version = None
@@ -28,3 +31,33 @@ def check_major_version(installed_version, expected_major_version):
 
   print("Installed Agent major version: {}".format(installed_major_version))
   return installed_major_version == expected_major_version
+
+def get_config_dir(agent_version):
+  """Get the agent configuration directory on *nix systems."""
+  if int(agent_version) == 5:
+    return "/etc/dd-agent"
+  else:
+    return "/etc/datadog-agent"
+
+
+def check_install_info(agent_version):
+  """Check install_info file."""
+  config_dir = get_config_dir(agent_version)
+  info_path = os.path.join(config_dir, "install_info")
+
+  if not os.path.isfile(info_path):
+    print("install_info file not found at {}".format(info_path))
+    return False
+
+  with open(info_path, 'r') as install_info:
+    contents = install_info.read()
+    for pat in [
+      r'^  tool:\s*saltstack$',
+      r'^  tool_version:\s*saltstack-([0-9.]+|unknown)$',
+      r'^  installer_version:\s*datadog_formula-[0-9](\.[0-9]+)*$'
+    ]:
+      if not re.search(pat, contents, flags=re.MULTILINE):
+        print("Expected match for '{}' in '{}'".format(pat, contents))
+        return False
+
+  return True
