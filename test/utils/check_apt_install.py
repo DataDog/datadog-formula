c63e6d8c0646cb9b2854b07ee2e3e237c4a7333b
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-10-20 14:43:26 +0200

    Lint python code (#93)
    * reformat check_apt_install.py
    
    * tests: check_apt_install: use assert
    
    * reformat check_yum_install.py
    
    * tests: check_yum_install: remove unused exception variable
    
    * tests: check_yum_install: use asserts
    
    * lint systemctl.py

--- test/utils/check_apt_install.py
+++ test/utils/check_apt_install.py
@@ -1,52 +1,48 @@
 #!/usr/bin/env python
 
-import apt, re, sys
+import apt
+import re
+import sys
 from helpers import get_options, check_major_version, check_install_info
 
+
 def get_apt_package_version(package_name):
-  cache = apt.cache.Cache()
-  cache.update()
-  cache.open()
+    cache = apt.cache.Cache()
+    cache.update()
+    cache.open()
 
-  installed_version = None
-  try:
-    pkg = cache["datadog-agent"]
-    if pkg.is_installed:
-      # pkg.installed has the form datadog-agent=1:x.y.z-1, we only want x.y.z
-      installed_version = re.match("datadog-agent=[0-9]+:(.*)-[0-9]+", str(pkg.installed)).groups()[0]
-  except KeyError:
-    # datadog-agent is not installed
-    pass
+    installed_version = None
+    try:
+        pkg = cache["datadog-agent"]
+        if pkg.is_installed:
+            # pkg.installed has the form datadog-agent=1:x.y.z-1, we only want x.y.z
+            installed_version = re.match("datadog-agent=[0-9]+:(.*)-[0-9]+", str(pkg.installed)).groups()[0]
+    except KeyError:
+        # datadog-agent is not installed
+        pass
 
-  return installed_version
+    return installed_version
 
 
 def main(argv):
-  expected_major_version = get_options(argv[1:])
-  print("Expected major version: {}".format(expected_major_version))
+    expected_major_version = get_options(argv[1:])
+    print("Expected major version: {}".format(expected_major_version))
 
-  installed_version = get_apt_package_version("datadog-agent")
-  print("Installed Agent version: {}".format(installed_version))
+    installed_version = get_apt_package_version("datadog-agent")
+    print("Installed Agent version: {}".format(installed_version))
 
-  result = check_major_version(installed_version, expected_major_version)
-  if result:
+    assert check_major_version(installed_version, expected_major_version)
     print("Agent version check successful!")
-  else:
-    print("Agent version check failed.")
-    sys.exit(1)
 
-  # expected_major_version
-  if expected_major_version:
-    if check_install_info(expected_major_version):
-      print("install_info check successful!")
+    # expected_major_version
+    if expected_major_version:
+        assert check_install_info(expected_major_version)
+        print("install_info check successful!")
     else:
-      print("install_info check failed.")
-      sys.exit(1)
-  else:
-    print("Skipping install_info check.")
+        print("Skipping install_info check.")
 
-  sys.exit()
+    sys.exit()
 
 
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
--- test/utils/check_yum_install.py
+++ test/utils/check_yum_install.py
@@ -1,59 +1,54 @@
 #!/usr/bin/env python
 
 import subprocess
-import yum, sys
+import yum
+import sys
 from helpers import get_options, check_major_version, check_install_info
 
+
 def get_yum_package_version(package_name):
-  yb = yum.YumBase()
-  try:
-    # Use next to stop at the first match
-    pkg = next(p for p in yb.rpmdb.returnPackages() if p.name == package_name)
-    installed_version = pkg.version
-  except StopIteration:
-    # datadog-agent is not in the list of installed packages
-    installed_version = None
-  
-  return installed_version
+    yb = yum.YumBase()
+    try:
+        # Use next to stop at the first match
+        pkg = next(p for p in yb.rpmdb.returnPackages() if p.name == package_name)
+        installed_version = pkg.version
+    except StopIteration:
+        # datadog-agent is not in the list of installed packages
+        installed_version = None
+
+    return installed_version
+
 
 def is_rpm_package_installed(package_name):
-  try:
-    subprocess.check_output(["rpm", "-q", package_name])
-    return True
-  except subprocess.CalledProcessError as e:
-    return False
+    try:
+        subprocess.check_output(["rpm", "-q", package_name])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
 
 def main(argv):
-  expected_major_version = get_options(argv[1:])
-  print("Expected major version: {}".format(expected_major_version))
+    expected_major_version = get_options(argv[1:])
+    print("Expected major version: {}".format(expected_major_version))
 
-  installed_version = get_yum_package_version("datadog-agent")
-  print("Installed Agent version: {}".format(installed_version))
+    installed_version = get_yum_package_version("datadog-agent")
+    print("Installed Agent version: {}".format(installed_version))
 
-  result = check_major_version(installed_version, expected_major_version)
-  if result:
+    assert check_major_version(installed_version, expected_major_version)
     print("Agent version check successful!")
-  else:
-    print("Agent version check failed.")
-    sys.exit(1)
 
-  # expected_major_version
-  if expected_major_version:
-    if check_install_info(expected_major_version):
-      print("install_info check successful!")
+    # expected_major_version
+    if expected_major_version:
+        assert check_install_info(expected_major_version)
+        print("install_info check successful!")
     else:
-      print("install_info check failed.")
-      sys.exit(1)
-  else:
-    print("Skipping install_info check.")
+        print("Skipping install_info check.")
 
-  if is_rpm_package_installed("gpg-pubkey-4172a230-55dd14f6"):
-    print("GPG key 4172a230 is installed, but shouldn't.")
-    sys.exit(1)
-  else:
+    assert not is_rpm_package_installed("gpg-pubkey-4172a230-55dd14f6")
     print("GPG key 4172a230 is not installed.")
 
-  sys.exit()
+    sys.exit()
+
 
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
